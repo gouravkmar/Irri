36a0a895f9907111766ff3765a843f8f4fd1fe6d
gouravsaavn <kumar.gourav3.14@gmail.com>
2023-05-31 18:21:17 +0530

    20 seconds lag

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(12, OUTPUT);
+  pinMode(14, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// the loop function runs over and over again forever
+void loop()
+{
+  digitalWrite(12, HIGH);
+  digitalWrite(14, HIGH);
+  // turn the LED on (HIGH is the voltage level)
+  delay(20000); // wait for a second
+  digitalWrite(12, LOW);
+  digitalWrite(14, LOW);
+  // turn the LED on (HIGH is the voltage level)
+  delay(20000); // wait for a second
 }
